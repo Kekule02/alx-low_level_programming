289a7f85dfe03819dd2b94a1d6512d58ac14e0de
Azeez Lawal <azeezlawal2002@gmail.com>
2022-04-08 17:31:10 +0000

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,12 @@
  */
 
 int main(void)
+  
 {
 char alphabet;
  
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-}   
+for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+{  
 putchar(alphabet);
 }
 putchar('\n');
