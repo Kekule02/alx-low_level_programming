1339813d7465460c6530e442d4a0652ec1d87c55
Azeez Lawal <azeezlawal2002@gmail.com>
2022-04-12 13:50:56 +0000

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Prints the sum of even-valued Fibonacci sequence
  *        terms not exceeding 4000000.
@@ -14,7 +13,7 @@ while (1)
 {
 fibsum = fib1 + fib2;
 if (fibsum > 4000000)
-break;      
+break;
 if ((fibsum % 2) == 0)
 tot_sum += fibsum;
 fib1 = fib2;
