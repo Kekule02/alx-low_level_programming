dc7b8a41c2b6749f0fd25fa1afcfd3dd14c6a8ca
Azeez Lawal <azeezlawal2002@gmail.com>
2022-06-01 15:29:46 +0000

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,6 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
 * append_text_to_file - Appends text at the end of a file.
 * @filename: A pointer to the name of the file.
